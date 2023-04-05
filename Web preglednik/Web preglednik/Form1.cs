ae7413d1b0e26e8282c1c921caeae3a70f78261e
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-04-05 09:31:28 +0200

    Add files via upload

---
+++ Web preglednik/Web preglednik/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_preglednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void btnNazad_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void bookmark_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void bookmarksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBook_Click_1(object sender, EventArgs e)
+        {
+            bookmarksToolStripMenuItem.DropDownItems.Add(webBrowser1.Url.ToString());
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate(textBox1.Text);
+            }
+        }
+
+        private void bookmarksToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
